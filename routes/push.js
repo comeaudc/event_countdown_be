249a7ee6dd034746f75bc7d816c710c584ff553b
DylanC <comeaudc@comcast.net>
2026-03-05 17:56:03 -0500

    Push Route started: must test

--- routes/push.js
+++ routes/push.js
@@ -1,7 +1,23 @@
-import express from 'express';
-import {tokenAuth} from '../middleware/tokenAuth.js';
-import Guest from '../models/guestModel.js';
+import express from "express";
+import { tokenAuth } from "../middleware/tokenAuth.js";
+import Guest from "../models/guestModel.js";
+import error from "../utilities/error.js";
 
 const router = express.Router();
+
+router.post("/", tokenAuth, async (req, res, next) => {
+  const { subscription } = req.body;
+
+  if (!subscription) next(error(400, "Subscription Required"));
+
+  try {
+    req.guest.pushSubscription = subscription;
+    await req.guest.save();
+    res.json({ message: "Push Subscription Saved" });
+  } catch (error) {
+    console.error(error);
+    next(error(500, "Server Error"));
+  }
+});
 
 export default router;
