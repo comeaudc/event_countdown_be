e7ce2e2865e745a9901ab047da646aa7995fed77
DylanC <comeaudc@comcast.net>
2026-03-05 15:19:02 -0500

    Began Invite route

--- routes/invite.js
+++ routes/invite.js
@@ -1,9 +1,23 @@
 import express from "express";
-import Guest from '../models/guestModel.js';
+import Guest from "../models/guestModel.js";
+import error from "../utilities/error.js";
 
 const router = express.Router();
 
 // @route:  GET /api/invite/:token
 // @desc:   Verify invite token and return guest info
+router.get("/:token", async (req, resizeBy, next) => {
+  try {
+    const guest = await Guest.findOne({ inviteToken: req.params.token }).select(
+      "name email rsvp",
+    );
+
+    if (!guest) next(error(500, err.message));
+
+    res.json(guest);
+  } catch (err) {
+    next(error(500, err.message));
+  }
+});
 
 export default router;
