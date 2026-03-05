6f68d11b9d4e88743793df8422bf2b42995f271b
DylanC <comeaudc@comcast.net>
2026-03-05 16:38:05 -0500

    Fixed Import Issue

--- middleware/basicMiddleware.js
+++ middleware/basicMiddleware.js
@@ -1,3 +1,5 @@
+import error from "../utilities/error.js";
+
 export const logReq = (req, _res, next) => {
   console.log(
     `${req.method} -- ${req.url} -- ${new Date().toLocaleTimeString()}`,
@@ -6,12 +8,12 @@ export const logReq = (req, _res, next) => {
   if (req.body) console.table(req.body);
 
   next();
-}
+};
 
 export const globalErr = (err, _req, res, _next) => {
   res.status(err.status || 500).json({ errors: [{ msg: err.message }] });
-}
+};
 
 export const notFound = (req, _res, next) => {
   next(error(404, "Resource not found"));
-}
+};
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -1,5 +1,25 @@
 import express from "express";
-
+import { tokenAuth } from "../middleware/tokenAuth.js";
+import error from "../utilities/error.js";
 const router = express.Router();
+
+// @route: POST /api/rsvp
+// @desc: Update RSVP
+router.post("/", tokenAuth, async (req, res, next) => {
+  const { attending } = req.body;
+  if (typeof attending !== "boolean")
+    next(error(400, "Attending must be boolean"));
+
+  try {
+    req.guest.rsvp = attending;
+
+    await req.guest.save();
+
+    res.json({ message: "RSVP Updated", rsvp: req.guest.rsvp });
+  } catch (err) {
+    console.error(err.message);
+    next(error(400, "Server Error"));
+  }
+});
 
 export default router;
